7c9131c1ed97ce4e9aa9bc316015ee41cb76a150
Piotr Kowalski <piecioshka@gmail.com>
2020-03-21 20:33:37 +0100

    Update selectors and assertion in e2e specs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('test-angular-services-testing app is running!');
+    expect(page.getTitleText()).toEqual('Photos');
   });
 
   afterEach(async () => {
